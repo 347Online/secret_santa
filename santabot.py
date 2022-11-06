5d206c6d7d76cec90cf3af46b24888486964d50f
347Online <38228372+347Online@users.noreply.github.com>
2022-11-05 21:56:32 -0500

    Import Discord SDK

--- santabot.py
+++ santabot.py
@@ -1,1 +1,19 @@
+import os
+
 import discord
+from dotenv import load_dotenv
+
+def main():
+  load_dotenv()
+  TOKEN = os.getenv('DISCORD_TOKEN')
+
+  client = discord.Client()
+
+  @client.event
+  async def on_ready():
+    print(f"{client.user} has connected")
+
+  client.run(TOKEN)
+
+if __name__ == "__main__":
+  main()
