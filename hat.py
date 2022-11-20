2c9594e70e643839d1d3d46093a8af2786cd1dcd
347Online <38228372+347Online@users.noreply.github.com>
2022-11-19 23:54:47 -0600

    Revised export
    Now uses output directory, which is .gitignore'd

--- hat.py
+++ hat.py
@@ -11,6 +11,17 @@ parser.add_argument("-n", "--names", help = "name of the file to use as the list
 parser.add_argument("-v", "--verbose", action = "store_true")
 args = parser.parse_args()
 
+# path = Path.cwd() / 'new' / 'hi' / 'there'
+# try:
+#     path.mkdir(parents=True, exist_ok=False)
+# except FileExistsError:
+#     print("Folder is already there")
+# else:
+#     print("Folder was created")
+
+# # print(Path('./output/' + args.names))
+# exit()
+
 def derange(ls) -> list:
   l = ls.copy()
   length = len(l)
@@ -33,20 +44,26 @@ def main() -> None:
   try:
     with open(args.names) as f:
       names = f.read().splitlines()
-      pairs = create_pairs(names)
-      if args.verbose:
-        print_pairs(pairs)
 
   except IOError:
     print(f"Could not open '{args.names}'")
+    exit()
+
+  pairs = create_pairs(names)
+  if args.verbose:
+    print_pairs(pairs)
   
+  path = Path.cwd() / 'output'
+  path.mkdir(parents=True, exist_ok=True)
+
   try:
-    fname = Path(args.names).stem + ".json"
+    fname = str(path / args.names).rsplit('.', 1)[0] + '.json'
     with open(fname, "w") as f:
-      f.write(json.dumps(pairs))
+      f.write(json.dumps(pairs, indent=2))
 
   except IOError:
     print(f"Could not write file '{fname}'")
+    exit()
   
 
 if __name__ == "__main__":
---
+++ thoughts.md
@@ -0,0 +1,6 @@
+1. ~~Create survey~~
+2. ~~Write a generic letter~~
+2. Get participants spreadsheet
+3. Assign pairings (shuffle names)
+4. Generate messages letting people know who their assigned recipient is, as well as their survey responses
+5. Send out the messages
