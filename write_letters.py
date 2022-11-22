ff286a913e85692054c4aec85f99cbbe8f4bdd02
347Online <38228372+347Online@users.noreply.github.com>
2022-11-21 22:44:23 -0600

    Writing letters finished
    Known bug with hat messing up the file destination. Maybe I'll fix it next year

--- write_letters.py
+++ write_letters.py
@@ -7,3 +7,48 @@
 # Your recipient is {RECIPIENT}
 
 # Their survey responses were {RESPONSES}
+
+import json
+import csv
+from pathlib import Path
+
+def write_letter(p, r, q):
+  response_string = ""
+  for key, value in q.items():
+    if key == "Discord Handle":
+      continue
+    response_string += key + ":\n" + value + "\n\n"
+
+  letter = f"""Seasons greetings, {p}!
+
+Your assigned recipient is {r}, here are their questionnaire responses:
+
+{response_string}
+Once again, thank you so much for participating in Secret Santa this year!
+
+Happy Holidays!!"""
+  
+  return letter
+
+
+
+def main():
+
+  with open("output/names.json") as f:
+    people = json.load(f)
+  
+  responses = {}
+  with open("responses.csv") as r:
+    for line in csv.DictReader(r):
+      name = line["Preferred Name"]
+      responses[name] = line
+
+  for participant, recipient in people.items():
+    response_dict = responses[recipient]
+
+    letter = write_letter(participant, recipient, response_dict)
+    with open("output/letters/" + participant + ".txt", "w+") as f:
+      f.write(letter)
+
+if __name__ == "__main__":
+  main()
